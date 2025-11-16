aa8d5ace3e00dd806918e36211405852e5cea0dd
tanishka2105 <tanishkachaudhary2105@gmail.com>
2025-11-16 18:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function todolist(){
+    window.open("todolist app/index todo.html")
+}
+function music(){
+    window.open("Music/music.html")
+}
+function quote(){
+    window.open("quote/quote.html")
+}
+function mood(){
+    window.open("mood tracker/mood tracker.html")
+}
+function journal(){
+    window.open("journal/journal.html")
+}
+function breathe(){
+    window.open("Brethe/breathe.html")
+}
